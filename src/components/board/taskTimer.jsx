dcaeb9a5be371388690cfb59a7abd59a1e2f4a62
Phuong LÊ <phuong.le@menincode.com>
2020-03-02 16:17:42 +0000

    fix timer. Take timer data from firestore, but use state for the display

--- src/components/board/taskTimer.jsx
+++ src/components/board/taskTimer.jsx
@@ -1,35 +1,31 @@
-import React from "react";
-import firebase from "../../services/firebase";
+import React, { useState } from "react";
 import useInterval from "../../tools/useInterval";
 import checkZero from "../../tools/checkZero";
 
-function handleInterval(task) {
-	let timer = task.timer;
-	timer[2] = timer[2] + 1;
-	if (timer[2] === 60) {
-		timer[1] = timer[1] + 1;
-		timer[2] = 0;
-	}
-
-	if (timer[1] === 60) {
-		timer[0] = timer[0] + 1;
-		timer[1] = 0;
-	}
-
-	if (timer[0] === 24) {
-		timer[0] = 0;
-	}
-	firebase
-		.firestore()
-		.collection("task")
-		.doc(task.id)
-		.set({ ...task, timer });
-}
-
 const TaskTimer = ({ task }) => {
-	useInterval(handleInterval, 1000, task);
+	const handleInterval = timer => {
+		timer[2] = timer[2] + 1;
+		if (timer[2] === 60) {
+			timer[1] = timer[1] + 1;
+			timer[2] = 0;
+		}
+
+		if (timer[1] === 60) {
+			timer[0] = timer[0] + 1;
+			timer[1] = 0;
+		}
+
+		if (timer[0] === 24) {
+			timer[0] = 0;
+		}
+		const newTimer = [...timer]
+		setTimer(newTimer);
+	};
+
+	const [timer, setTimer] = useState(task.timer);
+	useInterval(handleInterval, 1000, timer);
 	let i = 0;
-	return task.timer.map((time, index) => {
+	return timer.map((time, index) => {
 		i++;
 		let separator = i < 3 ? ":" : "";
 		return (
